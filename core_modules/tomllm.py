c81e1f2884f7445540d08937e85c3c9679b5a340
MickaelRoger <mickael@mickael-roger.com>
2024-12-29 03:05:34 +0100

    Refacto LLM Processing to support others LLM + add DeepSeek LLM

--- core_modules/tomllm.py
+++ core_modules/tomllm.py
@@ -35,10 +35,10 @@ class TomLLM():
 #      self.llm = self.callGemini
 #      self.llm_model = "gemini-1.5-flash"
 #
-#    elif global_config['global']['llm'] == "deepseek":
-#      self.llm_client = OpenAI(api_key=global_config['global']["deepseek"]["api"], base_url="https://api.deepseek.com")
-#      self.llm = self.callOpenai
-#      self.llm_model = "deepseek-chat"
+    elif global_config['global']['llm'] == "deepseek":
+      self.llm_client = OpenAI(api_key=global_config['global']["deepseek"]["api"], base_url="https://api.deepseek.com")
+      self.llm = self.callDeepseek
+      self.llm_model = "deepseek-chat"
 
     else:
       print(f"LLM {global_config['global']['llm']} not supported")
@@ -101,6 +101,33 @@ class TomLLM():
     return True, values
 
 
+  def callDeepseek(self, messages, tools=None):
+
+
+    print("\n\nTools1********************")
+    print(tools)
+    print("*****************\n\n")
+
+    if tools: 
+      newtools = copy.deepcopy(tools)
+      for tool in newtools:
+        print("\n\nTool********************")
+        print(tool)
+        print("*****************\n\n")
+        if not tool["function"]["parameters"]:
+          del tool["function"]["parameters"]
+
+    else:
+      newtools = None
+
+    print("\n\nTools2********************")
+    print(newtools)
+    print("*****************\n\n")
+
+    return self.callOpenai(messages, newtools)
+
+
+
   def callOpenai(self, messages, tools=None):
 
     print("---------1----------")
@@ -157,26 +184,33 @@ class TomLLM():
 
   def generateContextPrompt(self, input, lang, position):
   
+    gps = "" 
+
     current_timezone = ZoneInfo("Europe/Paris")
     if position is not None:
       tf = TimezoneFinder()
       current_timezone = ZoneInfo(tf.timezone_at(lat=position['latitude'], lng=position['longitude']))
+      gps = f"My actual GPS position is: \nlatitude: {position['latitude']}\nlongitude: {position['longitude']}."
   
   
     today= datetime.now(current_timezone).strftime("%A %d %B %Y %H:%M:%S")
     weeknumber = datetime.now().isocalendar().week
-    todayMsg = {"role": "system", "content": f"Today is {today}. Week number is {weeknumber}.\n\n"}
-  
+    todayMsg = {"role": "system", "content": f"Today is {today}. Week number is {weeknumber}. {gps}\n\n"}
+
+    svc_context = ""
+    for service in self.services:
+      if self.services[service]['service_context'] != "":
+        svc_context = f"{svc_context}\n{self.services[service]['service_context']}\n"
+    
+
+
+
     if self.history: 
       self.history[0] = todayMsg
     else:
       self.history.append(todayMsg)
-  
-    if len(self.history) == 1: 
       self.history.append({"role": "system", "content": self.tom_context})
-  
-    if position is not None:
-      self.history.append({"role": "system", "content": f"My actual GPS position is: \nlatitude: {position['latitude']}\nlongitude: {position['longitude']}"})
+      self.history.append({"role": "system", "content": f"{svc_context}"})
 
 
     #behaviors = self.services['behavior']['obj'].behavior_get()
@@ -204,22 +238,22 @@ class TomLLM():
       modules_name_list.append(module)
 
 
-    triage_tools = [
+    tools = [
       {
         "type": "function",
         "function": {
           "name": "modules_needed_to_answer_user_prompt",
-          "description": "Get information about a train station. For a train, subway or tram station, this function returns its ID, name, city and all the lines of the station",
+          "description": "This function is used to execute the appropriate module to get the required data to answer the user's request",
           "strict": True,
           "parameters": {
             "type": "object",
             "properties": {
               "modules_name": {
-                "type": "array",
-                "items": {
+                #                "type": "array",
+                #                "items": {
                   "type": "string",
                   "enum": modules_name_list,
-                },
+                #                },
                 "description": f"List of module names",
               },
             },
@@ -230,120 +264,99 @@ class TomLLM():
       },
     ]
 
-    conversation = []
-    conversation.append({"role": "system", "content": self.tom_context})
+    conversation = copy.deepcopy(self.history) 
   
-    if position is not None:
-      conversation.append({"role": "system", "content": f"My actual GPS position is: \nlatitude: {position['latitude']}\nlongitude: {position['longitude']}"})
-
-    svc_context = ""
-    for service in self.services:
-      if self.services[service]['service_context'] != "":
-        svc_context = f"{svc_context}\n{self.services[service]['service_context']}"
-    
-    conversation.append({"role": "system", "content": f"{svc_context}"})
-
     tooling = json.dumps(available_tools)
-    conversation.append({"role": "system", "content": f"Here is a list of modules. For each module, you have the its description. Your role is to call the function 'modules_needed_to_answer_user_prompt' with the list of modules needed to provide me the answer to my request. 'module_name' is not a name of a function, it's a value of the parameter of the 'modules_needed_to_answer_prompt'. You must never use 'module_name' as a function name.\n{tooling}"})
+    #    conversation.append({"role": "system", "content": f"Here is a list of available modules. Your role is to identify the necessary module(s) to meet the user's request. To do so, you must call the function 'modules_needed_to_answer_user_prompt' with the list of required modules as a parameter. If you are able to answer the user request without any modules, do it and do not call 'modules_needed_to_answer_user_prompt' function.\n\n{tooling}"})
+    conversation.append({"role": "system", "content": f"Here is a list of modules. For each module, you have the its description. Your role is to call the function 'modules_needed_to_answer_user_prompt' with the module needed to provide me the answer to my request. 'module_name' is not a name of a function, it's a possible value of the parameter of the 'modules_needed_to_answer_prompt'. You must never use the field 'module_name' as a function name.\n{tooling}"})
+
+    # Alternative to test: As a language model, you cannot respond to all of my requests. Therefore, you might need additional information. Certain information or functionalities can be found in modules. You can load these modules to assist you in responding by using the load_module function. Below, you will find a complete list of modules along with their descriptions.
 #
-#
-    # Create a message history with only 'user' and 'assistant' role messages
-    for message in self.history:
-      if isinstance(message, dict):     
-        if "role" in message.keys():
-          if message['role'] in ["assistant", "user"]:
-            conversation.append(message)
-#
+    while True:
 
-    ret, triage = self.llm(messages=conversation, tools=triage_tools)
+      ret, response = self.llm(messages=conversation, tools=tools)
 
-    if ret:
-      if triage.choices[0].finish_reason == "stop":
-        self.history.append({"role": triage.choices[0].message.role, "content": triage.choices[0].message.content})
-        return True, triage.choices[0].message.content
-
-      elif triage.choices[0].finish_reason == "tool_calls":
-        load_modules= []
-        for tool_call in triage.choices[0].message.tool_calls:
-          if tool_call.function.name.find("modules_needed_to_answer_user_prompt") != -1:    # Probably bad prompt, but sometimes it calls 'module_name.modules_needed_to_answer_user_prompt'
-            mods = json.loads(tool_call.function.arguments)
-            for mod in mods['modules_name']:
-              load_modules.append(mod)
-
-        print("Load: " + str(load_modules))
-
-
-        tools = []
-        for mod in list(set(load_modules)):
-          tools = tools + self.services[mod]['tools']
-
-        print("Tools: " + str(tools))
+      conversation = copy.deepcopy(self.history)
   
-        while True:
+      if ret:
+        if response.choices[0].finish_reason == "stop":
+          self.history.append({"role": response.choices[0].message.role, "content": response.choices[0].message.content})
+          return True, response.choices[0].message.content
+  
+        elif response.choices[0].finish_reason == "tool_calls":
 
-          
-          ret, response = self.llm(messages=self.history, tools=tools)
-  
-          if not ret:
-            return False, f"Error: {response}"
-  
-          if response.choices[0].finish_reason == "stop":
-            self.history.append({"role": response.choices[0].message.role, "content": response.choices[0].message.content})
-            return True, response.choices[0].message.content
+          load_modules= []
+          # Are we in triage?
+          for tool_call in response.choices[0].message.tool_calls:
+            if tool_call.function.name.find("modules_needed_to_answer_user_prompt") != -1:    # Probably bad prompt, but sometimes it calls 'module_name.modules_needed_to_answer_user_prompt'
+              mod = json.loads(tool_call.function.arguments)
+              #for mod in mods['modules_name']:
+              load_modules.append(mod['modules_name'])
+            # Todo add something to check if the function name is a module, if so, it's an LLM error and we must use it as if it was a parameter
+            if tool_call.function.name in modules_name_list:
+              load_modules.append(tool_call.function.name)
 
-  
-          elif response.choices[0].finish_reason == "tool_calls":
 
- 
-            self.history.append(response)
+
+          # Yes we are
+          if load_modules:
+            print("Load: " + str(load_modules))
   
+            tools = []
+            for mod in load_modules:
+              tools = tools + self.services[mod]['tools']
+  
+            print("Tools: " + str(tools))
+
+          # We are not
+          else:
+            
+            conversation.append(response.choices[0].message)
+
             responseContext = {"functions": [], "rules": []}
-  
+    
             for tool_call in response.choices[0].message.tool_calls:
-  
+    
               function_name = tool_call.function.name
               function_params = json.loads(tool_call.function.arguments)
-  
+    
               print("Call: " + str(function_name) + " with " + str(function_params))
-  
+    
                 # Memory
                 #if function_name == "tom_keep_current_conversation_in_memory":
                 #  res, msg = userList[username]['services']['memory'].history_keep(userList[username]['history'], username)
-  
+    
                 #  if res:
                 #    userList[username].reset()
                 #    return msg
                 #  else:
                 #    return "Error while keeping our conversation"
                   
-  
+    
                 # End of memory
               res, function_result = self.functions[function_name]['function'](**function_params)
-  
+    
               if res is False:
-                self.history.append({"role": 'tool', "content": "Error", "tool_call_id": tool_call.id})
+                self.history.append({"role": 'assistant', "content": "Error while executing the function call"})
                 return False, f"Error while executing the function {function_name}"
-  
+    
 
-              self.history.append({"role": 'tool', "content": json.dumps(function_result), "tool_call_id": tool_call.id})
+  
+              conversation.append({"role": 'tool', "content": json.dumps(function_result), "tool_call_id": tool_call.id})
                 
               # TODODODODODDODODO
               if function_name not in responseContext['functions']:
                 responseContext['functions'].append(function_name)
                 responseContext['rules'].append({"role": "system", "content": self.functions[function_name]['responseContext']})
               
+    
+            conversation = conversation + responseContext['rules']
   
-            self.history = self.history + responseContext['rules']
-
-          else:
-            return False, "Bad finish reason"
-
-      else:
-        return False, f"Bad triage"
-  
-  
-    else: 
-      return False, "Error, no response from LLM"
+        else:
+          return False, f"Bad finish reason"
+    
+      else: 
+        return False, "Error, no response from LLM"
 
 
 
